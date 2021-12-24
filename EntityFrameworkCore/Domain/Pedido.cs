f6ac7a338c63ce3cb9ff4f511ed13589b1a3ae65
Bryan <bryanlima200@outlook.com>
2021-12-23 22:37:58 -0300

    Adicionado entidades de domínio

---
+++ EntityFrameworkCore/Domain/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using EntityFrameworkCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
+        public TipoFreteEnum TipoFrete { get; set; }
+        public StatusPedidoEnum StatusPedido { get; set; }
+        public string Observacao { get; set; }
+        public ICollection<PedidoItem> Itens { get; set; }
+    }
+}
---
+++ EntityFrameworkCore/Domain/Produto.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string CodigoBarras { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public TipoProdutoEnum TipoProduto { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
