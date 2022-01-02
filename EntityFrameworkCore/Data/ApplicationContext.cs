0816efdcb63eeec88fbdb37d006c0b73af582104
Bryan <bryanlima200@outlook.com>
2022-01-02 20:51:01 -0300

    Adicionado exibição de log no console

--- EntityFrameworkCore/Data/ApplicationContext.cs
+++ EntityFrameworkCore/Data/ApplicationContext.cs
@@ -1,6 +1,7 @@
 ﻿using EntityFrameworkCore.Data.Configurations;
 using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,13 +10,17 @@ namespace EntityFrameworkCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(iLoggingBuilder => iLoggingBuilder.AddConsole());
+
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Cliente { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-B76722G\\SQLEXPRESS; Initial Catalog=EFCore; User ID=developer; Password=dev*10; Integrated Security=True; Persist Security Info=False; Pooling=False; MultipleActiveResultSets=False; Encrypt=False; Trusted_Connection=False");
+            optionsBuilder.UseLoggerFactory(_logger)
+                          .EnableSensitiveDataLogging()
+                          .UseSqlServer("Data Source=DESKTOP-B76722G\\SQLEXPRESS; Initial Catalog=EFCore; User ID=developer; Password=dev*10; Integrated Security=True; Persist Security Info=False; Pooling=False; MultipleActiveResultSets=False; Encrypt=False; Trusted_Connection=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
