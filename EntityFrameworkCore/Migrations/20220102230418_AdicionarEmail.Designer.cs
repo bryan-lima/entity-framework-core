2ca60010cc16a23b830ed92d4f8a577c85d350ff
Bryan <bryanlima200@outlook.com>
2022-01-02 20:07:32 -0300

    Adicionado migration AdicionarEmail

--- EntityFrameworkCore/Migrations/20220102230418_AdicionarEmail.Designer.cs
+++ EntityFrameworkCore/Migrations/20220102230418_AdicionarEmail.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220102230418_AdicionarEmail")]
+    partial class AdicionarEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
